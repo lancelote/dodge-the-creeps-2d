23c584bd9c8b92dcc7918d0832e8fe563d5d607b
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-03-25 22:50:50 +0100

    add `start_game` signal to hud

---
+++ hud.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+# notifies `Main` node that the "Start" button was pressed
+signal start_game
+
+
+func _ready() -> void:
+	pass
+
+
+
+func _process(delta: float) -> void:
+	pass
