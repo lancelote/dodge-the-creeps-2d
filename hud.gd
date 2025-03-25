468008ebc9054541a064683ea0a05672d475b29b
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-03-25 22:57:41 +0100

    add a function to handle game over logic

--- hud.gd
+++ hud.gd
@@ -8,3 +8,15 @@ func show_message(text: String) -> void:
 	$Message.text = text
 	$Message.show()
 	$MessageTimer.start()
+
+
+func show_game_over() -> void:
+	show_message("Game Over")
+	await $MessageTimer.timeout
+	
+	$Message.text = "Dodge the Creeps!"
+	$Message.show()
+	
+	# wait for a second
+	await get_tree().create_timer(1.0).timeout
+	$StartButton.show()
